86db76009cf271baa85ac4d05c583996e3e33139
Nguyễn Nguyên Giáp <103838138+megait004@users.noreply.github.com>
2024-03-27 00:18:56 +0700

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,18 @@
+
+/*/while(di?u ki?n l?p){
+    kh?i l?nh
+						}
+							*/
+#include<iostream>
+using namespace std;
+int main(){
+	int n ;
+	cin >> n;
+ while(n<1){
+ 	cin >> n;
+ }
+ if(n>=1&&n<=99){
+ 	cout << n;
+ }
+	return 0;
+}
